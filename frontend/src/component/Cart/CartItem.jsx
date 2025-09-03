30029e1324dfdba762cec78aa926e492fc790d44
divyasingla123 <divyasingla05@gmail.com>
2025-09-03 14:27:29 +0530

    success

--- frontend/src/component/Cart/CartItem.jsx
+++ frontend/src/component/Cart/CartItem.jsx
@@ -1,4 +1,8 @@
+import { IconButton } from '@mui/material'
 import React from 'react'
+import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import Chip from '@mui/material/Chip';
 
 const CartItem = () => {
   return (
@@ -11,10 +15,26 @@ const CartItem = () => {
             <div className='flex items-center justify-between lg:w-[70%]'>
                 <div className='space-y-1 lg:space-y-3 w-full'>
                     <p>biryani</p>
+                    <div className='flex justify-between items-center'>
+                        <div className='flex items-center space-x-1'>
+<IconButton>
+    <RemoveCircleOutlineIcon/>
+</IconButton>
+<div className='w-5 h-5 text-xs flex itmes-center justify-center'>
+    {5}
+</div>
+<IconButton>
+    <AddCircleOutlineIcon/>
+</IconButton>
+                        </div>
+                    </div>
                 </div>
+                <p>₹1946</p>
             </div>
         </div>
-      
+      <div className='pt-3 space-x-3'>
+         {[1,1,1,].map((item)=><Chip label={"bread"}/>)}
+      </div>
     </div>
   )
 }
