ea2a94202642945b28b9a2ed938413fb305be18f
adwitajindal <adwita.jindal.42@gmail.com>
2025-09-07 19:36:51 +0530

    upto reducer.js in state > restaurant

--- frontend/src/component/State/store.js
+++ frontend/src/component/State/store.js
@@ -1,9 +1,11 @@
 import { authReducer } from "./Authentication/Reducer";
 import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import { thunk } from "redux-thunk";
+import restaurantReducer from "./Restaurant/Reducer";
 
 const rootReducer = combineReducers({
   auth: authReducer,
+  restaurant:restaurantReducer
 });
 
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
--- frontend/src/component/config/logic.js
+++ frontend/src/component/config/logic.js
@@ -1,6 +1,6 @@
 export const isPresentInFavorites=(favorites,restaurantId)=>{
     for(let item of favorites){
-        if(restaurant.id==item.id){
+        if(restaurantId===item.id){
             return true;
         }
     }
