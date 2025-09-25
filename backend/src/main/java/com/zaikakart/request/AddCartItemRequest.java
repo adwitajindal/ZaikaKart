6f27c396bb88d2d433b9ce862f2f98423aaea014
divyasingla123 <divyasingla05@gmail.com>
2025-09-25 20:37:46 +0530

    success

---
+++ backend/src/main/java/com/zaikakart/request/AddCartItemRequest.java
@@ -0,0 +1,11 @@
+package com.zaikakart.request;
+
+
+import java.util.List;
+
+public class AddCartItemRequest {
+
+    private Long foodId;
+    private int quantity;
+    private List<String> ingredients;
+}
---
+++ backend/src/main/java/com/zaikakart/service/CartService.java
@@ -0,0 +1,23 @@
+package com.zaikakart.service;
+
+import com.zaikakart.model.Cart;
+import com.zaikakart.model.CartItem;
+import com.zaikakart.model.User;
+import com.zaikakart.request.AddCartItemRequest;
+
+public interface CartService {
+
+    public CartItem addToCart(AddCartItemRequest req, String jwt)throws Exception;
+
+    public CartItem updateCartItemQuantity(Long cartItemId,int quantity)throws Exception;
+
+    public Cart removeItemFromCart(Long cartItemId,String jwt)throws Exception;
+
+    public Long calculateCartTotals(Cart cart)throws Exception;
+
+    public Cart findCartById(Long id)throws Exception;
+
+    public Cart findCartByUserId(Long userId) throws Exception;
+
+    public Cart 
+}
